2850a8953e2e60f2dc3b9952985a8f2e619b188a
Indradip <chadsigmarizzler@gmail.com>
2025-03-11 20:56:10 +0600

    first commit

---
+++ list.py
@@ -0,0 +1,20 @@
+empty = []
+
+print(empty)
+print(type(empty))
+
+numbers = [1,2,3,4,5,"Nooria", "Indradip"]
+print(numbers)
+print(numbers[5])
+print(numbers[6])
+
+numbers.append("NahianDiddy")
+print(numbers)
+
+numbers.pop()
+numbers.pop()
+numbers.pop()
+print(numbers)
+
+aList = [100,200,300,400,500]
+print(aList[::-1])
---
+++ play with lsit.py
@@ -0,0 +1,18 @@
+l = [4,5,1,2,9,7,10,8]
+print(f"Original list : {l}")
+
+sum = 0
+for i in l: 
+    sum = sum + i
+
+print(f"sum = {sum}")
+
+avg = sum / len(l)
+print(f"average = {avg}\n\n")
+
+l.sort()
+print(f"Sorted list : {l}")
+
+print(f"Smallest element is : {l[0]}")
+print(f"Smallest element is : {l[-1]}")
+     
